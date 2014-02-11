e7d16d3d131ed8997e8f0d650f2c144fd934bf17
Michael Joseph Kramer <michael@sagehou.se>
2014-02-10 20:21:42 -0500

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global cbusjs, $*/
+
+
+window.cbusjs = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    cbusjs.init();
+});
